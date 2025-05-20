c5cb6c7a9dd7bf2464b07c4b6a9f37ef66ab4e00
Joao <joao-victor1982@hotmail.com>
2025-05-19 21:16:28 -0300

    Implement user profile management and enhance user CRUD functionality

--- recipesAPI/serializers/user_serializer.py
+++ recipesAPI/serializers/user_serializer.py
@@ -1,15 +1,45 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from recipesAPI.models import UserProfile
 from recipesAPI.validators import user_validators
 
 
-class CreateUserSerializer(serializers.ModelSerializer):
+class UserProfileSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(
+        max_length=20,
+        required=True,
+    )
+    last_name = serializers.CharField(
+        max_length=60,
+        required=False,
+    )
+
+    class Meta:
+        model = UserProfile
+        fields = [
+            'avatar', 'first_name', 'last_name',
+            'description', 'birth_date', 'favorite_recipe'
+        ]
+
+    def validate_first_name(self, value):
+        return user_validators.min_length(value)
+
+    def validate_last_name(self, value):
+        return user_validators.min_length(value)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+    email = serializers.EmailField(required=True)
+    profile = UserProfileSerializer(required=True)
+
     class Meta:
         model = User
-        fields = ['username', 'password', 'email']
-
-    email = serializers.EmailField(required=True)
+        fields = [
+            'id', 'username', 'password', 'email', 'profile',
+        ]
 
     def validate_username(self, value):
         return user_validators.validate_username(value)
@@ -21,30 +51,33 @@ class CreateUserSerializer(serializers.ModelSerializer):
         return user_validators.validate_email(value)
 
     def create(self, validated_data):
+        profile_data = validated_data.pop('profile')
         user = User.objects.create_user(**validated_data)
+        UserProfile.objects.create(user=user, **profile_data)
         return user
 
 
-class GetUserSerializer(serializers.ModelSerializer):
+class UpdateUserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer(required=False)
+    new_password = serializers.CharField(required=False, write_only=True)
+    password = serializers.CharField(required=True, write_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email',]
-
-
-class SecurityUserUpdateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'password', 'new_password']
-
-    new_password = serializers.CharField(write_only=True)
-    password = serializers.CharField(write_only=True)
+        fields = [
+            'username', 'password', 'new_password', 'email', 'profile'
+        ]
 
     def validate(self, attrs):
         user = self.context['request'].user
-        attrs['password'] = user_validators.current_password_check(
-            user,
-            attrs['password']
-        )
+        password = attrs.get('password')
+
+        if password:
+            user_validators.current_password_check(user, attrs['password'])
+        else:
+            raise serializers.ValidationError(
+                {'password': 'This field is required'}
+            )
         return super().validate(attrs)
 
     def validate_username(self, value):
@@ -53,37 +86,30 @@ class SecurityUserUpdateSerializer(serializers.ModelSerializer):
     def validate_new_password(self, value):
         return user_validators.validate_password(value)
 
-    def update(self, instance, validated_data):
-        new_password = validated_data.get('new_password')
-
-        if new_password:
-            validated_data.pop('password', None)
-            instance.set_password(new_password)
-        else:
-            validated_data.pop('password', None)
-            validated_data.pop('new_password', None)
-
-        return super().update(instance, validated_data)
-
-
-class ProfileUserUpdateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['email', 'first_name', 'last_name', 'password']
-
-    password = serializers.CharField(required=True, write_only=True)
-
-    def validate(self, attrs):
-        user = self.context['request'].user
-        attrs['password'] = user_validators.current_password_check(
-            user,
-            attrs['password']
-        )
-        return super().validate(attrs)
-
     def validate_email(self, value):
         return user_validators.validate_email(value)
 
     def update(self, instance, validated_data):
-        validated_data.pop('password', None)
-        return super().update(instance, validated_data)
+        user = self.context['request'].user
+
+        profile_data = validated_data.pop('profile', None)
+        new_password = validated_data.pop('new_password', None)
+        validated_data.pop('password')
+
+        if new_password:
+            instance.set_password(new_password)
+
+        for field, value in validated_data.items():
+            setattr(instance, field, value)
+
+        if profile_data:
+            if not user.profile:
+                UserProfile.objects.create(user=user, **profile_data)
+            else:
+                for field, value in profile_data.items():
+                    setattr(instance.profile, field, value)
+            instance.profile.save()
+
+        instance.save()
+
+        return instance
--- recipesAPI/views/user.py
+++ recipesAPI/views/user.py
@@ -1,55 +1,76 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.exceptions import MethodNotAllowed
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-from recipesAPI.permissions import IsSelf
-from recipesAPI.serializers import (CreateUserSerializer, GetUserSerializer,
-                                    ProfileUserUpdateSerializer,
-                                    SecurityUserUpdateSerializer)
+from recipesAPI.serializers import UpdateUserSerializer, UserSerializer
 
 
-class UserRegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = CreateUserSerializer
+class UserViewSet(ModelViewSet):
+    serializer_class = UserSerializer
+    http_method_names = ['get', 'post', 'patch', 'delete']
 
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+    def initial(self, request, *args, **kwargs):
+        super().initial(request, *args, **kwargs)
 
-        return Response(
-            {'message': 'User created Successfully'},
-            status=status.HTTP_201_CREATED
-        )
+        forbidden = ['GET', 'PATCH', 'DELETE']
+        endswith_me = request.path.endswith('/me/') or \
+            request.path.endswith('/me')
+        endswith_me = request.path.rstrip('/').endswith('/me/')
 
+        if request.method in forbidden and not endswith_me:
+            raise MethodNotAllowed(
+                method=request.method,
+                detail='For GET, PATCH, DELETE use /api/user/me'
+            )
 
-class GetUserView(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = GetUserSerializer
-    permission_classes = [IsAuthenticated, IsSelf]
-    lookup_field = 'pk'
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
 
+        return [permission() for permission in permission_classes]
 
-class ProfileUserUpdate(generics.UpdateAPIView):
-    queryset = User.objects.all()
-    serializer_class = ProfileUserUpdateSerializer
-    permission_classes = [IsAuthenticated, IsSelf]
-    lookup_field = 'pk'
+    def get_queryset(self):
+        return User.objects.filter(pk=self.request.user.pk)
 
-
-class SecurityUserUpdate(generics.UpdateAPIView):
-    queryset = User.objects.all()
-    serializer_class = SecurityUserUpdateSerializer
-    permission_classes = [IsAuthenticated, IsSelf]
-    lookup_field = 'pk'
-
-
-class DeleteUserView(generics.DestroyAPIView):
-    queryset = User.objects.all()
-    permission_classes = [IsAuthenticated, IsSelf]
-    lookup_field = 'pk'
+    def get_object(self):
+        return self.request.user
 
     def perform_destroy(self, instance):
         # Send email confirmation
         return super().perform_destroy(instance)
+
+    @action(
+        detail=False,
+        url_path='me',
+        methods=['get', 'patch', 'delete'],
+        permission_classes=[IsAuthenticated],
+        serializer_class=UpdateUserSerializer,
+    )
+    def me(self, request):
+        user = request.user
+
+        if request.method == 'GET':
+            serializer = self.get_serializer(user)
+            return Response(serializer.data)
+
+        elif request.method == 'PATCH':
+            serializer = self.get_serializer(
+                user,
+                data=request.data,
+                partial=True,
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+            return Response(serializer.data)
+
+        elif request.method == 'DELETE':
+            self.perform_destroy(user)
+
+            return Response(status.HTTP_204_NO_CONTENT)
